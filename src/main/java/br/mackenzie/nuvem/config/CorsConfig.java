b1b2ce41e896d4060feff53e0a23c6017a4549db
mtalala <superraioi2014@gmail.com>
2026-05-14 16:52:37 -0300

    chore: update CorsConfig

--- src/main/java/br/mackenzie/nuvem/config/CorsConfig.java
+++ src/main/java/br/mackenzie/nuvem/config/CorsConfig.java
@@ -11,7 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:3000"
+                        "http://localhost:3000",
+                        "http://35.170.203.182:3000"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
