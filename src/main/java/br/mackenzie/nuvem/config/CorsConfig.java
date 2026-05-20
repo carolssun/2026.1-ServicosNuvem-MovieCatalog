96c517ba5df07a2b137bdd1f68fad6f2eee26e53
mtalala <superraioi2014@gmail.com>
2026-05-20 20:38:46 -0300

    refactor: update EC2 address

--- src/main/java/br/mackenzie/nuvem/config/CorsConfig.java
+++ src/main/java/br/mackenzie/nuvem/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:3000",
-                        "http://3.86.104.121:3000"
+                        "http://44.206.251.163:3000"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
